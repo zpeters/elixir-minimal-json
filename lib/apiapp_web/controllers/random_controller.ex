c8e2e200cb2aab140192c963a345cf3b93e0d9c5
Zach Peters <zpeters@gmail.com>
2022-08-12 08:41:34 -0500

    removed unnecessary line

--- lib/apiapp_web/controllers/random_controller.ex
+++ lib/apiapp_web/controllers/random_controller.ex
@@ -3,7 +3,6 @@ defmodule ApiappWeb.RandomController do
 
   def index(conn, _params) do
     things = %{"one" => 1, "two" => 2}
-    # render(conn, "index.json", things: things)
     json(conn, things)
   end
 end
